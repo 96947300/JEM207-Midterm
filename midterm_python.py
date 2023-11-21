486907432b3cc62d60789d48afcff7e076342d21
Caglar Aydin <96947300@fsv.cuni.cz>
2023-11-21 13:35:05 +0100

    change

---
+++ midterm_python.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 13:21:50 2023
+
+@author: caglaraydin
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.DataFrame({'number':[0,1],'name':['AB','BC']})
